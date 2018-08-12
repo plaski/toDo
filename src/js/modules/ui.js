3f115a3f8deafda6ff0722e7de6a6de88ce1c11a
plaski <maciejploskonka@gmail.com>
2018-08-12 23:47:52 +0200

    edited date change

--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -18,15 +18,13 @@ const UI = {
     const html =
       `<li class="${item.done ? `list-group-item-success` : ''} ${item.important ? `border border-danger` : ''} my-1 list-group-item rounded d-flex justify-content-between flex-wrap" id="item-${item.id}">
         <div class="w-100 mr-2 mb-3 align-self-start">${item.text}</div>
-        <div class="ml-auto d-flex flex-column justify-content-between align-items-end">
+        <hr class="w-100">
+        <div class="w-100 d-flex flex-row align-items-end justify-content-between">
+          <small class="">${createdDate}</small>
           <div class="btn-group" role="group">
             <button class="btn btn-sm btn-outline-success" id="doneBtn"><i class="fa fa-check" aria-hidden="true"></i></button>
             <button class="btn btn-sm btn-outline-info" id="editBtn"><i class="fa fa-pencil" aria-hidden="true"></i></button>
             <button class="btn btn-sm btn-outline-danger" id="deleteBtn"><i class="fa fa-times" aria-hidden="true"></i></button>
-          </div>
-          <div class="d-flex flex-column mt-1">
-            <small class="align-self-end">${editedDate ? `edited: ${editedDate}` : ``}</small>
-            <small class="align-self-end">created: ${createdDate}</small>
           </div>
         </div>
       </li>`;
@@ -85,9 +83,9 @@ const UI = {
     const span = input.parentNode;
     span.innerText = inputValue;
     input.remove();
-    const editedDateSpan = document.querySelector('#item-' + item.id).children[1].children[1].children[0];
+    const editedDateSpan = document.querySelector('#item-' + item.id).children[2].children[0];
     editedDate = this.formatDate(editedDate);
-    editedDateSpan.innerText = `edited: ${editedDate}`;
+    editedDateSpan.innerText = `${editedDate}`;
     return inputValue;
   },
 
